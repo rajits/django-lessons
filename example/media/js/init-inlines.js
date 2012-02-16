03938ebace34664eebf3bdff27470912cdb72e8e
Rajit Sarkar <rsarkar@celerity.com>
2012-02-16 15:57:45 -0500

    Tabs now use js, so that clicking on one will not reload the page

--- example/media/js/init-inlines.js
+++ example/media/js/init-inlines.js
@@ -1,6 +1,25 @@
+function showtab(num)
+{
+    $(".page").hide();
+    $(".page" + num).show();
+
+    // clear/ set which is active
+    $(".tab").removeClass("selected");
+    $("#tab" + num).addClass("selected");
+}
+
 $(function() {
-	// Is this a new or existing object?
-	if ($("div#content > h1").text().search("Add") > -1) {
-		$("#id_lessonrelation_set-0-content_type option").eq(26).attr('selected', 'selected');
-	}
-})
+    if ($("div#content > h1").text().search("Add") > -1) {
+        // This is a new object - set default values
+        var selectBox = $("#id_lessonrelation_set-0-content_type");
+        selectBox.val("26").attr('selected', 'selected');
+
+        $("#id_lessonrelation_set-0-object_id").val("1");
+    }
+
+    showtab(0);
+
+    $(".tab").click(function() {
+        showtab(this.id.replace(/[^\d]/g, ""));
+    });
+});
--- lessons/templatetags/lessons_tags.py
+++ lessons/templatetags/lessons_tags.py
@@ -2,22 +2,14 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='active_tab')
-def active_tab(request):
-    full_path = request.get_full_path()
-    search_str = 'active_tab='
-    start = full_path.find(search_str)
-    if start > -1:
-        return full_path[start+len(search_str):]
-    else:
-        return '1'
+tabs = (
+    ('Overview', 'Directions', 'Objectives', 'Background'),
+    ('Global Metadata', 'Time and Date Metadata'),
+)
 
-@register.filter(name='display_in_tab')
-def display_in_tab(fieldset, request):
-    tabs = [
-        ('Overview', 'Directions', 'Objectives', 'Background'),
-        ('Global Metadata', 'Time and Date Metadata'),
-    ]
-    tab = active_tab(request)
-    index = int(tab) - 1 # list is zero-based
-    return fieldset.name in tabs[index]
+@register.filter(name='tab_num')
+def tab_num(fieldset):
+    for count, fieldsets in enumerate(tabs):
+        if fieldset.name in fieldsets:
+            return count
+    return 0
