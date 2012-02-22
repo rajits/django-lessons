126dd92365bb7e844277b774bd6658de498b393c
Rajit Sarkar <rsarkar@celerity.com>
2012-02-22 16:33:43 -0500

    minor change to url patterns

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('lessons.views',
-    url(r'^activities/(?P<slug>.+)/$', 'activity_detail'),
-    url(r'^lessons/(?P<slug>.+)/$', 'lesson_detail'),
+    url(r'^activity/(?P<slug>.+)/$', 'activity_detail'),
+    url(r'^lesson/(?P<slug>.+)/$', 'lesson_detail'),
 #    url(r'^lessons/(?P<slug>.+)/assessments/$', 'lesson_detail', {'template_name': 'lessons/fragments/assessments.html'}),
 #    url(r'^lessons/(?P<slug>.+)/learning-objectives/$', 'lesson_detail', {'template_name': 'lessons/fragments/learning_objectives.html'}),
 #    url(r'^lessons/(?P<slug>.+)/background-information/$', 'lesson_detail', {'template_name': 'lessons/fragments/background_information.html'}),
