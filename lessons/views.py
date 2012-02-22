9ef0e71d705a6ef30418d9a5ca4f64fb31a94d1f
Rajit Sarkar <rsarkar@celerity.com>
2012-02-22 09:49:38 -0500

    Added initial template for lesson pages

--- lessons/views.py
+++ lessons/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponseForbidden, Http404
 from django.template import RequestContext
 from django.utils.translation import ugettext, ugettext_lazy as _
 
+from settings import RELATION_MODELS, REQUIRED_FIELDS
 from lessons.models import Activity, Lesson
 
 def activity_detail(request, slug, template_name='lessons/activity_detail.html'):
@@ -21,6 +22,20 @@ def lesson_detail(request, slug, template_name='lessons/lesson_detail.html'):
     else:
         activities = lesson.get_activities()
 
-    return render_to_response(template_name, {
+    context = {
         'lesson': lesson,
-    }, context_instance=RequestContext(request))
+        'activities': activities,
+    }
+
+    for field in REQUIRED_FIELDS:
+        related_ctypes = lesson.get_related_content_type(field[0])
+        if len(related_ctypes) > 0:
+            context[field[0]] = related_ctypes[0].content_object
+
+    for model in RELATION_MODELS:
+        name = model.split('.')[1]
+        related_ctypes = lesson.get_related_content_type(name)
+        if len(related_ctypes) > 0:
+            context[name] = related_ctypes[0].content_object
+
+    return render_to_response(template_name, context, context_instance=RequestContext(request))
