c1ea33567144e52e37b8a31d195f204db8c0f1b1
Rajit Sarkar <rsarkar@celerity.com>
2012-02-23 11:26:11 -0500

    add templates for remaining roll ups

--- lessons/urls.py
+++ lessons/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('lessons.views',
     url(r'^lesson/(?P<slug>[-\w]*)/$', 'lesson_detail'),
     url(r'^lesson/(?P<slug>[-\w]*)/subjects/$', 'lesson_detail', {'template_name': 'lessons/fragments/subjects.html'}),
     url(r'^lesson/(?P<slug>[-\w]*)/learning-objectives/$', 'lesson_detail', {'template_name': 'lessons/fragments/learning_objectives.html'}),
-    url(r'^lesson/(?P<slug>[-\w]*)/other_notes/$', 'lesson_detail', {'template_name': 'lessons/fragments/other_notes.html'}),
-    url(r'^lesson/(?P<slug>[-\w]*)/background-information/$', 'lesson_detail', {'template_name': 'lessons/fragments/bg_information.html'}),
+    url(r'^lesson/(?P<slug>[-\w]*)/other-notes/$', 'lesson_detail', {'template_name': 'lessons/fragments/other_notes.html'}),
+    url(r'^lesson/(?P<slug>[-\w]*)/background-information/$', 'lesson_detail', {'template_name': 'lessons/fragments/bg_info.html'}),
 )
