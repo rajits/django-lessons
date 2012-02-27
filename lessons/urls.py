f5729ea5044b55a09ba5b9de0833a5400ceb6b91
Rajit Sarkar <rsarkar@celerity.com>
2012-02-27 12:00:47 -0500

    consolidated roll-ups by tab

--- lessons/urls.py
+++ lessons/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('lessons.views',
     url(r'^activity/(?P<slug>[-\w]*)/$', 'activity_detail'),
     url(r'^lesson/(?P<slug>[-\w]*)/$', 'lesson_detail'),
-    url(r'^lesson/(?P<slug>[-\w]*)/subjects/$', 'lesson_detail', {'template_name': 'lessons/fragments/subjects.html'}),
-    url(r'^lesson/(?P<slug>[-\w]*)/learning-objectives/$', 'lesson_detail', {'template_name': 'lessons/fragments/learning_objectives.html'}),
-    url(r'^lesson/(?P<slug>[-\w]*)/other-notes/$', 'lesson_detail', {'template_name': 'lessons/fragments/other_notes.html'}),
+    url(r'^lesson/(?P<slug>[-\w]*)/objectives/$', 'lesson_detail', {'template_name': 'lessons/fragments/objectives.html'}),
+    url(r'^lesson/(?P<slug>[-\w]*)/materials/$', 'lesson_detail', {'lessons/fragments/materials.html'}),
+    url(r'^lesson/(?P<slug>[-\w]*)/preparation/$', 'lesson_detail', {'template_name': 'lessons/fragments/preparation.html'}),
     url(r'^lesson/(?P<slug>[-\w]*)/background-information/$', 'lesson_detail', {'template_name': 'lessons/fragments/bg_info.html'}),
 )
