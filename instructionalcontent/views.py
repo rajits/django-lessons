c661654ffbbc4e27d289451a08d7d0f72c15966e
Rajit Sarkar <rsarkar@celerity.com>
2012-03-16 16:43:48 -0400

    added ajax views for importing text

--- instructionalcontent/views.py
+++ instructionalcontent/views.py
@@ -39,3 +39,28 @@ def lesson_detail(request, slug, template_name='lessons/lesson_detail.html'):
             context[name] = related_ctypes[0].content_object
 
     return render_to_response(template_name, context, context_instance=RequestContext(request))
+
+def background_information(request, id):
+    lesson = get_object_or_404(Lesson, id=id)
+    background_information = ''
+
+    for activity in lesson.get_activities():
+        background_information += activity.background_information
+
+    context = { 'background_information': background_information, }
+    return render_to_response('lessons/background_information.html',
+                              context,
+                              context_instance=RequestContext(request))
+
+def learning_objectives(request, id):
+    lesson = get_object_or_404(Lesson, id=id)
+    learning_objectives = []
+
+    for activity in lesson.get_activities():
+        learning_objectives += ul_as_list(activity.learning_objectives)
+    deduped_objectives = set(learning_objectives)
+
+    context = { 'learning_objectives': list(learning_objectives), }
+    return render_to_response('lessons/learning_objectives.html',
+                              context,
+                              context_instance=RequestContext(request))
