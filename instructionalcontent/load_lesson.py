544f783e16442ee820f900c6f5f393ad4285b5bb
Rajit Sarkar <rsarkar@celerity.com>
2012-03-16 16:41:32 -0400

    moved lesson fixture to separate file (to prevent circular references)

---
+++ instructionalcontent/load_lesson.py
@@ -0,0 +1,20 @@
+from instructionalcontent.models import Activity, Lesson, LessonActivity
+
+a, created = Activity.objects.get_or_create(slug='drawing-political-borders')
+a2, created = Activity.objects.get_or_create(slug='comparing-political-borders')
+
+l = Lesson(title='Political Borders', slug='political-borders', id_number='0417')
+l.appropriate_for = 1
+l.subtitle_guiding_question = 'Why are the borders of countries located in certain places?'
+l.description = '''Students think about regions and borders by determining where they would place borders in 
+                an artificial continent, based on a set of physical and cultural features of the area.'''
+l.duration = 100
+l.other_notes = 'This is lesson 1 in a series of 10 lessons in a unit on Europe.'
+l.save()
+
+lr = LessonActivity(lesson=l, activity=a)
+lr.transition_text = "Make sure you have groups' completed maps from Lesson 1, Activity 1 in the Beyond Borders unit"
+lr.save()
+
+lr2 = LessonActivity(lesson=l, activity=a2)
+lr2.save()
--- instructionalcontent/utils.py
+++ instructionalcontent/utils.py
@@ -1,5 +1,4 @@
 from BeautifulSoup import BeautifulSoup
-from models import *
 
 def truncate(string, limit):
     return string[:limit] + (string[limit:] and '...')
@@ -7,23 +6,3 @@ def truncate(string, limit):
 def ul_as_list(html):
     soup = BeautifulSoup(html)
     return [li.contents[0] for li in soup('li')]
-
-def load_lesson():
-    a, created = Activity.objects.get_or_create(slug='drawing-political-borders')
-    a2, created = Activity.objects.get_or_create(slug='comparing-political-borders')
-
-    l = Lesson(title='Political Borders', slug='political-borders')
-    l.appropriate_for = 1
-    l.subtitle_guiding_question = 'Why are the borders of countries located in certain places?'
-    l.description = '''Students think about regions and borders by determining where they would place borders in 
-                    an artificial continent, based on a set of physical and cultural features of the area.'''
-    l.duration = 100
-    l.other_notes = 'This is lesson 1 in a series of 10 lessons in a unit on Europe.'
-    l.save()
-
-    lr = LessonActivity(lesson=l, activity=a)
-    lr.transition_text = "Make sure you have groups' completed maps from Lesson 1, Activity 1 in the Beyond Borders unit"
-    lr.save()
-
-    lr2 = LessonActivity(lesson=l, activity=a2)
-    lr2.save()
