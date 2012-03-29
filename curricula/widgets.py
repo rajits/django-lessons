d26e2fc63bed67686970cb8061195e4b191be6fe
Rajit Sarkar <rsarkar@celerity.com>
2012-03-29 09:01:04 -0400

    vocabulary widget needs to check for empty value

--- curricula/widgets.py
+++ curricula/widgets.py
@@ -15,14 +15,16 @@ class VocabularyIdWidget(widgets.ForeignKeyRawIdWidget):
         super(VocabularyIdWidget, self).__init__(rel, attrs)
 
     def render(self, name, value, attrs=None):
-        src = settings.STATIC_URL + 'sites/education/i/ico_ee.png'
-
         output = [super(VocabularyIdWidget, self).render(name, value, attrs)]
-        try:
-            encyclopedic = GlossaryTerm.objects.get(id=value).encyclopedic
-            if encyclopedic:
-                output.append(u'<a href="/admin/edu_core/encyclopedicentry/%s/" target="_blank">' % encyclopedic.id)
-                output.append(u'<img src="%s" alt="Encyclopedic Entry available"/></a>' % src)
-        except GlossaryTerm.DoesNotExist:
-            pass
+
+        if value:
+            try:
+                encyclopedic = GlossaryTerm.objects.get(id=value).encyclopedic
+                if encyclopedic:
+                    src = settings.STATIC_URL + 'sites/education/i/ico_ee.png'
+
+                    output.append(u'<a href="/admin/edu_core/encyclopedicentry/%s/" target="_blank">' % encyclopedic.id)
+                    output.append(u'<img src="%s" alt="Encyclopedic Entry available"/></a>' % src)
+            except GlossaryTerm.DoesNotExist:
+                pass
         return mark_safe(u''.join(output))
